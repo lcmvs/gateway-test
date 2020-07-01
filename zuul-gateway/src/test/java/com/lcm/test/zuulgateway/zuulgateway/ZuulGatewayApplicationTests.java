2b7b6cb45e230f84bc0ea4640e916cf44e155376
longchenming <1264221821@qq.com>
2020-07-01 17:35:36 +0800

    init zuul

---
+++ zuul-gateway/src/test/java/com/lcm/test/zuulgateway/zuulgateway/ZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcm.test.zuulgateway.zuulgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
